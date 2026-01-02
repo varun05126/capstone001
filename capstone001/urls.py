073531a23edd3bd8b155baf6100e40a72c9c9d4e
varun <malthumkarvarun@gmail.com>
2026-01-02 23:06:04 +0530

    Update urls.py

--- capstone001/urls.py
+++ capstone001/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from aiapp.views import home, download_pdf
 
 urlpatterns = [
-    path("", home),
-    path("download-pdf/", download_pdf),
+    path("admin/", admin.site.urls),
+    path("", home, name="home"),
+    path("download/", download_pdf, name="download_pdf"),
 ]
